89a68d68f1b409371c653dedce931e8563672a00
Polar Bear <anasanalyst7039@gmail.com>
2026-08-29 00:01:06 +0300

    Improve error handling and return logic in process.py
    Refactor error handling and streamline return statements.

--- Project_2/process.py
+++ Project_2/process.py
@@ -10,22 +10,16 @@ def count_duplicates():
         return
     try:
         df = pd.read_excel(input_file)
-        
         if df.empty:
             print("الملف فارغ!")
-            return
-            
+            return   
         column_name = df.columns[0]
-        
         df_counts = df[column_name].value_counts().reset_index()
-        
         df_counts.columns = [column_name, 'Count (التكرار)']
-        
         df_counts.to_excel(output_file, index=False)
         print(f"تم بنجاح! تم إنشاء ملف التكرارات باسم: {output_file}")
         
     except Exception as e:
         print(f"حدث خطأ أثناء معالجة الملف: {e}")
-
 if __name__ == '__main__':
     count_duplicates()
